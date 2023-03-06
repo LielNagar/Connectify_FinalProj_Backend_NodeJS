01c1feb96d3c21a18d0e231ed439175f6fdf6f81
LielNagar <104072448+LielNagar@users.noreply.github.com>
2023-03-07 01:30:01 +0200

    nothing much

--- src/app.js
+++ src/app.js
@@ -7,30 +7,30 @@ const upload = multer({
     fileSize: 1000000,
   },
   fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|JPG)$/)) {
       return cb(new Error("Please upload a .jpg/.jpeg/.png file"));
     }
     return cb(undefined, true);
   },
 });
-const cors = require('cors');
+const cors = require("cors");
 
 const User = require("./models/user");
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors())
-app.get("/users/:id", async (req,res)=>{
-    console.log('here:', req.params.id)
-    try{
-        const user = await User.findOne({id: req.params.id})
-        if(!user) throw new Error();
-        res.send(user)
-    }catch(e){
-        res.send(e)
-    }
-})
+app.use(cors());
+app.get("/users/:id", async (req, res) => {
+  console.log("here:", req.params.id);
+  try {
+    const user = await User.findOne({ id: req.params.id });
+    if (!user) throw new Error();
+    res.send(user);
+  } catch (e) {
+    res.send(e);
+  }
+});
 
 app.post("/user", async (req, res) => {
   try {
@@ -42,36 +42,31 @@ app.post("/user", async (req, res) => {
   }
 });
 
-app.post(
-  `/users/:id/avatar`,
-  upload.single("avatar"),
-  async (req, res) => {
-    try{
-      const buffer = await sharp(req.file.buffer)
+app.post(`/users/:id/avatar`, upload.single("avatar"), async (req, res) => {
+  try {
+    const buffer = await sharp(req.file.buffer)
       .resize({ width: 250, height: 250 })
       .png()
       .toBuffer();
     const user = await User.findOne({ id: req.params.id });
     user.avatar = buffer;
-    user.save();
-    res.send();
-    }catch(e){
-      res.status(400).send({ error: e.message });
-    }
+    await user.save();
+    res.status(200).send();
+  } catch (e) {
+    res.status(400).send({ error: e.message });
   }
-);
+});
 
-app.get('/users/:id/avatar', async (req,res)=>{
-    try{
-        const user= await User.findOne({id: req.params.id})
-        if(!user || !user.avatar) throw new Error()
-        res.set('Content-Type','image/png')
-        res.send(user.avatar)
-    }catch(e){
-        res.status(404).send()
-    }
-})
-
+app.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findOne({ id: req.params.id });
+    if (!user || !user.avatar) throw new Error();
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
 
 app.listen(port, () => {
   console.log("Server is up on port", port);
